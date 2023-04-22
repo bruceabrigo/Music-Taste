bae4449cfda61b65061797da39c62eb1c9e40027
bruceabrigo <bruceabrigo@outlook.com>
2023-04-21 20:55:53 -0500

    Users can now log into spotify, no data yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react"
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
+
+const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            let parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {})
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [spotifyToken, setSpotifyToken] = useState("")
+    const [loggedIn, setLoggedIn] = useState(false)
+    useEffect(() => {
+        console.log('this was derived from the URL: ', getTokenFromUrl())
+        const spotifyToken = getTokenFromUrl().access_token
+        window.location.has = "";
+        console.log('this is our spotify token: ', spotifyToken)
+    
+        if (spotifyToken) {
+            setSpotifyToken(spotifyToken)
+            setLoggedIn(true)
+        }
+    })
+    return (
+        <div className="App">
+            {!loggedIn && <a href='http://localhost:8888'>Login to Spotify</a>}
+            {loggedIn && (
+                <>
+                    <div>Logged In! Nice Brah</div>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
